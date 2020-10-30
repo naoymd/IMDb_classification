9b352077dcb4e3fb084208ab7e1b519a8f6d338d
naoymd <nyamada@aoki-medialab.jp>
2020-10-30 14:34:36 +0900

    2020/10/30

--- rnn/my_attention.py
+++ rnn/my_attention.py
@@ -1,23 +1,49 @@
 import math
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
 
+from typing import Dict, Any, Tuple
+
+
+def draw_heatmap(data_map, row_labels, column_labels, save_dir=None, name=None):
+    fig, ax = plt.subplots(figsize=(20, 1))
+    heatmap = ax.pcolor(data_map, cmap=plt.cm.Blues)
+
+    ax.set_xticks(np.arange(data_map.shape[1]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data_map.shape[0]) + 0.5, minor=False)
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    ax.set_xticklabels(row_labels, minor=False, rotation=90)
+    ax.set_yticklabels(column_labels, minor=False)
+    plt.savefig(os.path.join(save_dir, name + '.png'), bbox_inches='tight')
+    plt.close()
+
+
 class Attention(nn.Module):
-    def __init__(self, dimension, args):
+    def __init__(self, dimension: int, **kwargs: Dict[str, Any]):
         super(Attention, self).__init__()
-        self.qkv_linear = args.attention_qkv_linear
+        self.qkv_linear = kwargs.get('qkv_linear', True)
         self.query_linear = nn.Linear(dimension, dimension)
         self.key_linear = nn.Linear(dimension, dimension)
         self.value_linear = nn.Linear(dimension, dimension)
 
-        self.mask = args.attention_mask
-        self.scaled = args.scaled_dot_product
+        self.mask = kwargs.get('mask', False)
+        self.scaled = kwargs.get('scaled', True)
         self.scale = 1.0 / math.sqrt(dimension)
         self.softmax = nn.Softmax(dim=-1)
 
-        self.output_mode = args.attention_output
+        self.output_mode = kwargs.get('output_mode', '')
+        self.conv = nn.Conv1d(
+            dimension*2, dimension,
+            kernel_size=1, stride=1, dilation=1, padding=0, groups=2
+        )
+        self.bn = nn.BatchNorm1d(dimension)
+        self.pool = nn.AdaptiveAvgPool1d(dimension)
         self.cat_linear = nn.Linear(dimension*2, dimension)
         self.relu = nn.ReLU()
         self.output_linear = nn.Linear(dimension, dimension)
@@ -40,7 +66,7 @@ class Attention(nn.Module):
         mask = None
         return mask
 
-    def forward(self, input, memory, mask=None):
+    def forward(self, input: torch.Tensor, memory: torch.Tensor, mask=None) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Inputs:
             input : (batch_size, output_size, dimension) or (batch_size, dimension)
@@ -54,10 +80,12 @@ class Attention(nn.Module):
             -> attention map
         """
         query = input
-        key = value = memory
+        key = memory
+        value = memory
 
         query_squeeze = False
         if len(query.size()) == 2:
+            input = input.unsqueeze(dim=1)
             query = query.unsqueeze(dim=1)
             query_squeeze = True
 
@@ -99,13 +127,16 @@ class Attention(nn.Module):
             add : output と query の和
         """
         output = torch.matmul(attention_map, value)
-        if self.output_mode == 'cat':
-            output = torch.cat([output, query], dim=2)
-            output = self.cat_linear(output)
+        if self.output_mode == 'concat':
+            output = torch.cat([output, input], dim=2)
+            output = self.pool(F.relu(self.bn(self.conv(output.permute(0, 2, 1))).permute(0, 2, 1)))
+            # output = self.cat_linear(output)
             output = self.relu(output)
         elif self.output_mode == 'add':
-            output = self.relu(output + query)
+            output = self.relu(output + input)
+            # output = self.output_linear(output)
         else:
+            # output = self.output_linear(output)
             pass
         output = self.output_linear(output)
         output = self.tanh(output)
@@ -121,11 +152,34 @@ class Attention(nn.Module):
 
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, dimension, args):
+    def __init__(self, dimension: int, **kwargs: Dict[str, Any]):
         super(MultiheadAttention, self).__init__()
-        self.multihead_attention = nn.MultiheadAttention(dimension, num_heads=8)
+        num_heads = kwargs.get('num_heads', 4)
+        self.qkv_linear = kwargs.get('qkv_linear', True)
+        self.query_linear = nn.Linear(dimension, dimension)
+        self.key_linear = nn.Linear(dimension, dimension)
+        self.value_linear = nn.Linear(dimension, dimension)
+        self.multihead_attention = nn.MultiheadAttention(dimension, num_heads)
+        self.output_mode = kwargs.get('output_mode', '')
+        self.conv = nn.Conv1d(
+            dimension*2, dimension,
+            kernel_size=1, stride=1, dilation=1, padding=0, groups=2
+        )
+        self.bn = nn.BatchNorm1d(dimension)
+        self.pool = nn.AdaptiveAvgPool1d(dimension)
+        self.cat_linear = nn.Linear(dimension*2, dimension)
+        self.relu = nn.ReLU()
+        self.output_linear = nn.Linear(dimension, dimension)
+        
+        self.tanh = nn.Tanh()
 
-    def forward(self, input, memory):
+        init.xavier_uniform_(self.query_linear.weight)
+        init.xavier_uniform_(self.key_linear.weight)
+        init.xavier_uniform_(self.value_linear.weight)
+        init.xavier_uniform_(self.cat_linear.weight)
+        init.xavier_uniform_(self.output_linear.weight)
+
+    def forward(self, input: torch.Tensor, memory: torch.Tensor, mask=None) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Inputs:
             input : (batch_size, output_size, dimension) or (batch_size, dimension)
@@ -139,12 +193,19 @@ class MultiheadAttention(nn.Module):
             -> attention map
         """
         query = input
-        key = value = memory
+        key = memory
+        value = memory
 
         query_squeeze = False
         if len(query.size()) == 2:
+            input = input.unsqueeze(dim=1)
             query = query.unsqueeze(dim=1)
             query_squeeze = True
+
+        if self.qkv_linear:
+            query = self.query_linear(query)
+            key = self.key_linear(key)
+            value = self.value_linear(value)
 
         query = query.permute(1, 0, 2)
         key = key.permute(1, 0, 2)
@@ -157,7 +218,26 @@ class MultiheadAttention(nn.Module):
 
         output, attention_map = self.multihead_attention(query, key, value)
         output = output.permute(1, 0, 2)
-
+        """
+        output : (batch_size, output_size, dimension)
+        attention_map : (batch_size, output_size, queried_size)
+        (option)
+            cat : output と query のconcat
+            add : output と query の和
+        """
+        if self.output_mode == 'concat':
+            output = torch.cat([output, input], dim=2)
+            output = self.pool(F.relu(self.bn(self.conv(output.permute(0, 2, 1))).permute(0, 2, 1)))
+            # output = self.cat_linear(output)
+            output = self.relu(output)
+        elif self.output_mode == 'add':
+            output = self.relu(output + input)
+            # output = self.output_linear(output)
+        else:
+            # output = self.output_linear(output)
+            pass
+        # output = self.output_linear(output)
+        # output = self.tanh(output)
         if query_squeeze:
             output = output.squeeze(dim=1)
         """
