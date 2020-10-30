af455a58c90b4a3e3e6d3ff4ce51317f71aca946
naoymd <nyamada@aoki-medialab.jp>
2020-10-30 14:33:31 +0900

    2020/10/30

--- rnn/my_encoder.py
+++ rnn/my_encoder.py
@@ -1,40 +1,40 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from rnn.my_attention import *
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+"""
+Inputs:
+    input : (batch_size, sequence_length_size, input_size)
+    (inter_modal_h : (batch_size, hidden_size(hidden_size*2)))
+Outputs:
+    Bidirectional == False:
+        out, (c_out) : (batch_size, sequence_length_size, hidden_size)
+        h, (c) : (batch_size, hidden_size) = out[:-1]
+    Bidirectional == True:
+        out, (c_out) : (batch_size, sequence_length_size, hidden_size*2)
+        h, (c) : (batch_size, hidden_size*2) = out[:-1]
+"""
 
 
 class GRU_Encoder(nn.Module):
-    def __init__(self, batch_size, input_size, args):
-        super(GRU_Encoder, self).__init__()
-        self.hidden_size = hidden_size = args.hidden_size
-        self.num_layers = args.num_layers
-        self.h0 = torch.zeros(batch_size, hidden_size).to(device)
-        self.inter_modal_h_linear = nn.Linear(hidden_size*2, hidden_size)
+    def __init__(self, input_size, hidden_size=512, num_layers=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.gru = nn.ModuleList()
         for i in range(self.num_layers):
             self.gru.append(nn.GRUCell(input_size, hidden_size))
             input_size = hidden_size
-        init.xavier_uniform_(self.inter_modal_h_linear.weight)
-        print(self.gru)
+        # print(self.gru)
 
-    def forward(self, input, inter_modal_h=None):
+    def forward(self, input, seq_length=None):
         seq_len = input.size(1)
-        if inter_modal_h is None:
-            h_in = [self.h0]*self.num_layers
-        else:
-            if input.size(0) != inter_modal_h.size(0):
-                # test batchでmodal間のサイズが違う分のinter_modal_hの複製(repeat_interleave, repeat, tile, expand)
-                # To Do
-                inter_modal_h = inter_modal_h.repeat_interleave(input.size(0), dim=0)
-                print(input.size(), inter_modal_h.size())
-            if inter_modal_h.size(1) == self.hidden_size:
-                pass
-            elif inter_modal_h.size(1) == self.hidden_size*2:
-                inter_modal_h = self.inter_modal_h_linear(inter_modal_h)
-            h_in = [inter_modal_h]*self.num_layers
+        h0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        h_in = [h0]*self.num_layers
         out = []
         for i in range(seq_len):
             h = input[:, i, :]
@@ -47,93 +47,103 @@ class GRU_Encoder(nn.Module):
 
 
 class Bidirectional_GRU_Encoder(nn.Module):
-    def __init__(self, batch_size, input_size, args):
-        super(Bidirectional_GRU_Encoder, self).__init__()
-        self.hidden_size = hidden_size = args.hidden_size
-        self.num_layers = args.num_layers
-        self.h0 = torch.zeros(batch_size, hidden_size).to(device)
+    def __init__(self, input_size, hidden_size=512, num_layers=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.gru_f = nn.ModuleList()
         self.gru_b = nn.ModuleList()
         for i in range(self.num_layers):
             self.gru_f.append(nn.GRUCell(input_size, hidden_size))
             self.gru_b.append(nn.GRUCell(input_size, hidden_size))
             input_size = hidden_size
-        print(self.gru_f)
-        print(self.gru_b)
+        # print(self.gru_f)
+        # print(self.gru_b)
 
-    def reverse_input(self, input):
-        reverse_input = torch.flip(input, [1])
-        return reverse_input
+    def reverse(self, x):
+        reverse_x = torch.flip(x, [1])
+        return reverse_x
 
-    def forward(self, input, inter_modal_h=None):
+    def forward(self, input, seq_length=None):
         seq_len = input.size(1)
-        if inter_modal_h is None:
-            h_f_in = h_b_in = [self.h0]*self.num_layers
-        else:
-            if input.size(0) != inter_modal_h.size(0):
-                # test batchでmodal間のサイズが違う分のinter_modal_hの複製(repeat_interleave, repeat, tile, expand)
-                # To Do
-                inter_modal_h = inter_modal_h.repeat_interleave(input.size(0), dim=0)
-                print(input.size(), inter_modal_h.size())
-            if inter_modal_h.size(1) == self.hidden_size:
-                h_f_in = h_b_in = [inter_modal_h]*self.num_layers
-            elif inter_modal_h.size(1) == self.hidden_size*2:
-                h_f_in = [inter_modal_h[:, :self.hidden_size]]*self.num_layers
-                h_b_in = [inter_modal_h[:, self.hidden_size:]]*self.num_layers
-        out = []
+        h0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        h_f_in = [h0] * self.num_layers
+        h_b_in = [h0] * self.num_layers
+        reverse_input = self.reverse(input)
+        out_f = []
+        out_b = []
         for i in range(seq_len):
             h_f = input[:, i, :]
-            h_b = self.reverse_input(h_f)
+            h_b = reverse_input[:, i, :]
             for j, (layer_f, layer_b) in enumerate(zip(self.gru_f, self.gru_b)):
                 h_f = layer_f(h_f, h_f_in[j])
                 h_b = layer_b(h_b, h_b_in[j])
                 h_f_in[j] = h_f
                 h_b_in[j] = h_b
             h = torch.cat([h_f, h_b], dim=1)
-            out.append(h)
-        out = torch.stack(out, dim=1)
+            out_f.append(h_f)
+            out_b.append(h_b)
+        out_f = torch.stack(out_f, dim=1)
+        out_b = torch.stack(out_b, dim=1)
+        out_b = self.reverse(out_b)
+        out = torch.cat([out_f, out_b], dim=2)
         return out, h
 
 
+class GRU_Encoder_(nn.Module):
+    def __init__(self, input_size, hidden_size=512, num_layers=1, bidirectional=False):
+        super().__init__()
+        if bidirectional:
+            self.hidden_size = hidden_size * 2
+        else:
+            self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.padidx = 0
+        self.gru = nn.GRU(
+            input_size,
+            hidden_size,
+            num_layers = num_layers,
+            bidirectional = bidirectional,
+            batch_first = True
+        )
+
+    def forward(self, input, seq_length=None):
+        input_size = input.size(0)
+        if seq_length is not None:
+            # input: PackedSequence of (batch_size, seq_length, input_size)
+            input = pack_padded_sequence(input, seq_length, batch_first=True, enforce_sorted=False)
+        output, h = self.gru(input)
+        h = h.permute(1, 0, 2).reshape(input_size, self.num_layers, -1)
+        # output: (batch_size, seq_length, self.hidden_size)
+        # h: (batch_size, num_layers, self.hidden_size)
+        if seq_length is not None:
+            out, lengths = pad_packed_sequence(output, batch_first=True, padding_value=self.padidx)
+            # lengths: (batch_size, 1, self.hidden_size)
+            lengths = lengths.reshape(-1, 1, 1).expand(-1, -1, self.hidden_size) - 1
+            # h: (batch_size, 1, self.hidden_size)
+            h = torch.gather(out, 1, lengths)
+        h = h.reshape(input_size, -1)
+        return output, h
+
+
 class LSTM_Encoder(nn.Module):
-    def __init__(self, batch_size, input_size, args):
-        super(LSTM_Encoder, self).__init__()
-        self.hidden_size = hidden_size = args.hidden_size
-        self.num_layers = args.num_layers
-        self.h0 = torch.zeros(batch_size, hidden_size).to(device)
-        self.c0 = torch.zeros(batch_size, hidden_size).to(device)
-        self.inter_modal_h_linear = nn.Linear(hidden_size*2, hidden_size)
-        self.inter_modal_c_linear = nn.Linear(hidden_size*2, hidden_size)
+    def __init__(self, input_size, hidden_size=512, num_layers=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.lstm = nn.ModuleList()
         for i in range(self.num_layers):
             self.lstm.append(nn.LSTMCell(input_size, hidden_size))
             input_size = hidden_size
-        init.xavier_uniform_(self.inter_modal_h_linear.weight)
-        init.xavier_uniform_(self.inter_modal_c_linear.weight)
-        print(self.lstm)
+        # print(self.lstm)
 
-    def forward(self, input, inter_modal_h=None):
+    def forward(self, input, seq_length=None):
         seq_len = input.size(1)
-        if inter_modal_h is None:
-            h_in = [self.h0]*self.num_layers
-            c_in = [self.c0]*self.num_layers
-        else:
-            inter_modal_h, inter_modal_c = inter_modal_h
-            if input.size(0) != inter_modal_h.size(0):
-                # test batchでmodal間のサイズが違う分のinter_modal_h, inter_modal_cの複製(repeat_interleave, repeat, tile, expand)
-                # To Do
-                inter_modal_h = inter_modal_h.repeat_interleave(input.size(0), dim=0)
-                inter_modal_c = inter_modal_c.repeat_interleave(input.size(0), dim=0)
-                print(input.size(), inter_modal_h.size(), inter_modal_c.size())
-            if inter_modal_h.size(1) == self.hidden_size:
-                pass
-            elif inter_modal_h.size(1) == self.hidden_size*2:
-                inter_modal_h = self.inter_modal_h_linear(inter_modal_h)
-                inter_modal_c = self.inter_modal_c_linear(inter_modal_c)
-            h_in = [inter_modal_h]*self.num_layers
-            c_in = [inter_modal_c]*self.num_layers
+        h0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        c0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        h_in = [h0]*self.num_layers
+        c_in = [c0]*self.num_layers
         out = []
-        c_out = []
         for i in range(seq_len):
             h = input[:, i, :]
             for j, layer in enumerate(self.lstm):
@@ -141,58 +151,42 @@ class LSTM_Encoder(nn.Module):
                 h_in[j] = h
                 c_in[j] = c
             out.append(h)
-            c_out.append(c)
         out = torch.stack(out, dim=1)
-        c_out = torch.stack(c_out, dim=1)
-        return (out, c_out), (h, c)
+        return out, (h, c)
 
 
 class Bidirectional_LSTM_Encoder(nn.Module):
-    def __init__(self, batch_size, input_size, args):
-        super(Bidirectional_LSTM_Encoder, self).__init__()
-        self.hidden_size = hidden_size = args.hidden_size
-        self.num_layers = args.num_layers
-        self.h0 = torch.zeros(batch_size, hidden_size).to(device)
-        self.c0 = torch.zeros(batch_size, hidden_size).to(device)
+    def __init__(self, input_size, hidden_size=512, num_layers=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.lstm_f = nn.ModuleList()
         self.lstm_b = nn.ModuleList()
         for i in range(self.num_layers):
             self.lstm_f.append(nn.LSTMCell(input_size, hidden_size))
             self.lstm_b.append(nn.LSTMCell(input_size, hidden_size))
             input_size = hidden_size
-        print(self.lstm_f)
-        print(self.lstm_b)
+        # print(self.lstm_f)
+        # print(self.lstm_b)
 
-    def reverse_input(self, input):
-        reverse_input = torch.flip(input, [1])
-        return reverse_input
+    def reverse(self, x):
+        reverse_x = torch.flip(x, [1])
+        return reverse_x
 
     def forward(self, input, inter_modal_h=None):
         seq_len = input.size(1)
-        if inter_modal_h is None:
-            h_f_in = h_b_in = [self.h0]*self.num_layers
-            c_f_in = c_b_in = [self.c0]*self.num_layers
-        else:
-            inter_modal_h, inter_modal_c = inter_modal_h
-            if input.size(0) != inter_modal_h.size(0):
-                # test batchでmodal間のサイズが違う分のinter_modal_h, inter_modal_cの複製(repeat_interleave, repeat, tile, expand)
-                # To Do
-                inter_modal_h = inter_modal_h.repeat_interleave(input.size(0), dim=0)
-                inter_modal_c = inter_modal_c.repeat_interleave(input.size(0), dim=0)
-                print(input.size(), inter_modal_h.size(), inter_modal_c.size())
-            if inter_modal_h.size(1) == self.hidden_size:
-                h_f_in = h_b_in = [inter_modal_h]*self.num_layers
-                c_f_in = c_b_in = [inter_modal_c]*self.num_layers
-            elif inter_modal_h.size(1) == self.hidden_size*2:
-                h_f_in = [inter_modal_h[:, :self.hidden_size]]*self.num_layers
-                h_b_in = [inter_modal_h[:, self.hidden_size:]]*self.num_layers
-                c_f_in = [inter_modal_c[:, :self.hidden_size]]*self.num_layers
-                c_b_in = [inter_modal_c[:, self.hidden_size:]]*self.num_layers
-        out = []
-        c_out = []
+        h0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        c0 = torch.zeros(input.size(0), self.hidden_size).to(input.device)
+        h_f_in = [h0] * self.num_layers
+        h_b_in = [h0] * self.num_layers
+        c_f_in = [c0] * self.num_layers
+        c_b_in = [c0] * self.num_layers
+        reverse_input = self.reverse(input)
+        out_f = []
+        out_b = []
         for i in range(seq_len):
             h_f = input[:, i, :]
-            h_b = self.reverse_input(h_f)
+            h_b = reverse_input[:, i, :]
             for j, (layer_f, layer_b) in enumerate(zip(self.lstm_f, self.lstm_b)):
                 h_f, c_f = layer_f(h_f, (h_f_in[j], c_f_in[j]))
                 h_b, c_b = layer_b(h_b, (h_b_in[j], c_b_in[j]))
@@ -202,8 +196,49 @@ class Bidirectional_LSTM_Encoder(nn.Module):
                 c_b_in[j] = c_b
             h = torch.cat([h_f, h_b], dim=1)
             c = torch.cat([c_f, c_b], dim=1)
-            out.append(h)
-            c_out.append(c)
-        out = torch.stack(out, dim=1)
-        c_out = torch.stack(c_out, dim=1)
-        return (out, c_out), (h, c)
+            out_f.append(h_f)
+            out_b.append(h_b)
+        out_f = torch.stack(out_f, dim=1)
+        out_b = torch.stack(out_b, dim=1)
+        out_b = self.reverse(out_b)
+        out = torch.cat([out_f, out_b], dim=2)
+        return out, (h, c)
+
+
+class LSTM_Encoder_(nn.Module):
+    def __init__(self, input_size, hidden_size=512, num_layers=1, bidirectional=False):
+        super().__init__()
+        if bidirectional:
+            self.hidden_size = hidden_size * 2
+        else:
+            self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.padidx = 0
+        self.lstm = nn.LSTM(
+            input_size,
+            hidden_size,
+            num_layers = num_layers,
+            bidirectional = bidirectional,
+            batch_first = True
+        )
+
+    def forward(self, input, seq_length=None):
+        input_size = input.size(0)
+        if seq_length is not None:
+            # input: PackedSequence of (batch_size, seq_length, input_size)
+            input = pack_padded_sequence(input, seq_length, batch_first=True, enforce_sorted=False)
+        output, (h, c) = self.lstm(input)
+        h = h.permute(1, 0, 2).reshape(input_size, self.num_layers, -1)
+        c = c.permute(1, 0, 2).reshape(input_size, self.num_layers, -1)
+        # output: (batch_size, seq_length, self.hidden_size)
+        # h, c: (batch_size, num_layers, self.hidden_size)
+        if seq_length is not None:
+            out, lengths = pad_packed_sequence(output, batch_first=True, padding_value=self.padidx)
+            # lengths: (batch_size, 1, self.hidden_size)
+            lengths = lengths.reshape(-1, 1, 1).expand(-1, -1, self.hidden_size) - 1
+            # h: (batch_size, 1, self.hidden_size)
+            h = torch.gather(out, 1, lengths)
+            c = c[:, -1, :]
+        h = h.reshape(input_size, -1)
+        c = c.reshape(input_size, -1)
+        return output, (h, c)
